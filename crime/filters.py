2e8f4b69a4da370db23fe312a760869193868cd9
fabrice <praise2lord16@gmail.com>
2025-03-27 00:01:15 +0200

    Final changes before Defence

--- crime/filters.py
+++ crime/filters.py
@@ -5,11 +5,11 @@ from .models import *
 
 class SuspectFilter(django_filters.FilterSet):
     suspectNID = CharFilter(field_name="suspectNID", lookup_expr='icontains')
-    # phone = CharFilter(field_name="phone", lookup_expr='icontains')
+    f_name = CharFilter(field_name="f_name", lookup_expr='icontains')
     # start_date = DateFilter(field_name="date_arrested", lookup_expr='gte')
     # end_date = DateFilter(field_name="date_arrested", lookup_expr='lte')    
     class Meta:
          model = Suspect
          fields = '__all__'
          exclude = ['cell','suspectimage','village','note','evidences','reporters','relation','dob','province','crime_rate','witness_rate','evidence_rate',
-         'district','f_name','l_name','date_arrested','gender','l_name','father_name','mother_name','phone']
+         'district','l_name','date_arrested','gender','l_name','father_name','mother_name','phone']
